5aeb953f83fc9b31942fda058587ddf64c497021
Alkis <a.gkreen@gmail.com>
2021-05-15 19:36:39 +0200

    first

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c3677c82-3114-4b67-8e1f-c27899eaf330';
+export const MODAL_CLOSE_SEC = 2.5;
